80e441b9810c3f26d5685215b4636099f64c6fd1
Owner <abhatt@stuy.edu>
2017-01-12 23:13:53 -0500

    added Deck.java

---
+++ Deck.java
@@ -0,0 +1,35 @@
+public class Deck {
+    public String[] cards;
+    public int cut;
+    public int marker;
+
+    public void Deck(int num) {
+	cards = new String[num*52];
+	int i = 0;
+	for (String val: {"A","2","3","4","5","6","7","8","9","10","J","Q","K"}) {
+	    for (String suit: {"d","c","h","s"}) {
+		cards[i] = val+suit;
+		i += 1;
+	    }
+	}
+	cut = 0;
+	marker = 0;
+    }
+    public void shuffle() {
+	int n = cards.length;
+        for (int i = 0; i < n; i++) {
+            int s = i + (int) (Math.random()*(n-i));
+            String temp = cards[i];
+            cards[i] = cards[s];
+            cards[s] = temp;
+        }
+	cut = cards.length*3/4;
+    }
+    public String deal() {
+	if (cut == marker) {
+	    shuffle();
+	    return deal();
+	}
+	marker +=1;
+	return cards[marker];
+    }
