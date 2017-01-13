bb1b93d8bf965fb24edc35b6c12d46ce38201752
abhatt349 <abhatt@stuy.edu>
2017-01-13 01:49:53 -0500

    Update Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -13,6 +13,11 @@ public class Dealer {
 	System.out.println("Dealer money: "+money+"\nCards: "+cards);
     }
     public int sum() {
+	int retInt = 0;
+	for (int i = 0; i < cards.length()-1; i += 2) {
+	    retInt += Deck.value(cards.substring(i, i+1));
+	}
+	return retInt;
     }
 	
 }
