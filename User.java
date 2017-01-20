8f43369d43e56d9f571a9bb6e8afea3ed5d08418
abhatt349 <abhatt@stuy.edu>
2017-01-20 09:46:41 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,17 @@
 public abstract class User {
-    
+    public ArrayList<Card> cards;
+    public void add(Card newCard) {
+	cards.add(newCard);
+    }
+    public abstract void print();
+    public int sum() {
+	int retInt = 0;
+	for (int i = 0; i < cards.size(); i++) {
+	    retInt += Deck.value(cards.get(i));
+	}
+	return retInt;
+    }
+    public void reset() {
+	cards.clear();
+    }
 }
