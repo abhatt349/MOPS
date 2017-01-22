ca6774f84a080438129196ab761824a2746232b7
haiyaoliu <haiyao11235@icloud.com>
2017-01-22 17:52:01 -0500

    main driver skeleton

--- Driver.java
+++ Driver.java
@@ -1,14 +1,34 @@
+
+import cs1.keyboard;
 public class Driver {
-    public static void menu() {
+    public static String menu() {
 	System.out.println("Blackjack Menu");
 	System.out.println("n:new game");
 	System.out.println("h:help");
 	System.out.println("q:quit");
     }
-    public static void main(String[] args) {
-	
+    
+    public static void response() {
+	String input = Keyboard.readString();
+	if(input.equals("n")) {
+	    //send to ai menu
+	}
+	else if (input.equals("h")) {
+	    System.out.println("paste your stuff here aryan");
+	}
+	else if (input.equals("q")) {
+	    System.out.println("quitting.");
+	    return;
+	}
+	else {
+	    System.out.println("That is not a valid response. Please pick one of the options from the menu.");
+	    System.out.println("-----------");
+	    menu();
+	    response();
+	}
     }
-    
-    
+    public static void main(String[] args) {
+	response();
+    }
 }
 	
