cedd409bb837f241b8ca74f92e2af4f3954cdc3f
abhatt349 <abhatt@stuy.edu>
2017-01-22 16:01:43 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -5,6 +5,15 @@ public class Card {
     public static String[] suits = {"c","d","h","s"};
     public Card(int v) {
 	value = v%13;
+	if ((v%13)<= 7) {
+	    value = Integer.parseInt(values[v%13]);
+	}
+	else if ((v%13) == 12) {
+	    value = 11;
+	}
+	else {
+	    value = 10;
+	}
 	name = values[v%13] + suits[v/13];
     }
     public String toString() {
