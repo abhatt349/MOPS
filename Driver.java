289701c58898be0985d03c53de2c08a4d8b04816
abhatt349 <abhatt@stuy.edu>
2017-01-23 02:36:54 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,20 +1,20 @@
 import cs1.Keyboard;
 import java.util.ArrayList;
 public class Driver {
-    public static void menu() {
+    public static void menu() { //first menu encountered when they enter the game
 	System.out.println("Blackjack Menu");
-	System.out.println("n:new game");
-	System.out.println("h:help");
-	System.out.println("q:quit");
-	System.out.print("player: ");
+	System.out.println("n:new game");  //start a game
+	System.out.println("h:help");      //instructions
+	System.out.println("q:quit");      //exit
+	System.out.print("player: ");      
     }
     
-    public static String response() {
+    public static String response() {         //asks for input
 	String input = Keyboard.readString();
-	if(input.equals("n")) {
-	    return ""; //merged ai menu
+	if(input.equals("n")) {               //if they want to start a game
+	    return ""; //merged ai menu       //exits response()
 	}
-	else if (input.equals("h")) {
+	else if (input.equals("h")) {         //if they ask for instructions
 	    System.out.println("The goal of blackjack is to beat the dealer's hand without going over 21.");
 	    System.out.println("Face cards are worth 10. Aces are worth 1 or 11, whichever makes a better hand.");
 	    System.out.println("Each player starts with two cards.");
@@ -25,42 +25,42 @@ public class Driver {
 	    System.out.println("Blackjack means you win 1.5 the amount of your bet.");
 	    System.out.println("Dealer will hit until his/her cards total 17 or higher.");
 	    System.out.print("------------------\nuser: ");
-	    menu();
-	    return response();
+	    menu();                   //reprints menu options
+	    return response();        //loops back around to menu
 	}
-	else if (input.equals("q")) {
+	else if (input.equals("q")) { //exits
 	    System.out.println("driver: quitting.");
 	    return "q";
 	}
-	else {
+	else {                        //accounts for user incompetence
 	    System.out.println("driver: invalid response. try again with a menu option");
 	    System.out.println("-----------");
-	    menu();
-	    return response();
+	    menu();                   //reprints menu options
+	    return response();        //loops back around to menu
 	}
 	//return "???";
     }
-    public static int ai() {
+    public static int ai() {          //asks user how many AI's they want to play with
 	System.out.println("driver: Enter a number of AI's to add (between 0 and 5 inclusive)");
 	System.out.print("player: ");
 	int input = Keyboard.readInt();
-	if(input < 0 || input > 5) {
+	if(input < 0 || input > 5) {  //accounts for user incompetence
 	    System.out.println("driver: invalid number. please input an integer between 1 and 5 inclusive");
-	    return ai();
+	    return ai();              //loops back around, asking for input again
 	}
-	return input;
+	return input;                 //returns user-inputted number
     }
     public static void main(String[] args) {
-	menu();
-	if(response().equals("q")) return;
+	menu();                                  //displays menu
+	if(response().equals("q")) return;       //exits if user wants to quit
 	int n = ai();
-	ArrayList<AI> u = new ArrayList<AI>(n);
-	for(int i = 0; i < n; i++) {
+	ArrayList<AI> u = new ArrayList<AI>(n);  //creates ArrayList of AI's
+	for(int i = 0; i < n; i++) {             //fills ArrayList
 	    AI aa = new AI(i);
 	    u.add(aa);
 	}
 	Blackjack b = new Blackjack(u);
-	b.game();
+	b.game();                                //calls game runner
     }
 }
 	
