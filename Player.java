df02935d96d3085253e8595ad492fb91e80ac681
abhatt349 <abhatt@stuy.edu>
2017-01-22 22:21:46 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -22,6 +22,15 @@ public class Player {
 	for (int i = 0; i < cards.size(); i++) {
 	    retInt += cards.get(i).value;
 	}
+	boolean ace = false;
+	for (int i = 0;i<cards.size();i++) {
+	    if (cards.get(i).value == 11) {
+		ace = true;
+	    }
+	}
+	if (retInt>21 && ace) {
+	    retInt -= 10;
+	}
 	return retInt;
     }
     public void reset() {
